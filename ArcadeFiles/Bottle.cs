fedfd17c837943f2bf92873e72d24ae36b56eee9
Artiom Matievschi <23185316+Matioma@users.noreply.github.com>
2020-02-25 14:56:18 +0100

    Added flying bottle

---
+++ ArcadeFiles/Bottle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using GXPEngine;
+using GXPEngine.Core;
+
+class Bottle:Unit
+{
+    public Bottle(string spriteSheet, int cols, int rows)
+    {
+        visuals = new AnimationSprite(spriteSheet, cols, rows, -1, false, false);
+        AddChild(visuals);
+        AddHitBox();
+
+        dx = -10;
+        dy = 5;
+    }
+
+    public void AddForce(Vector2 direction, float force) {
+        //Vector2 directionNormal = 
+
+
+    }
+
+    public void Update() {
+        base.Update();
+      
+        if (OnGround)
+        {
+            OnBottleBreak();
+        }
+    }
+
+    void OnBottleBreak() {
+        LateDestroy();
+        SpawnAcidPuddle();
+    }
+
+    void SpawnAcidPuddle() {
+        var slowEffect = new SlowEffect("Art/puddle.png", 2, 1);
+        slowEffect.SetPivotPoint(PivotPointPosition.BOTTOM);
+
+        slowEffect.SetXY(x, y - 20);
+        //slowEffect.SpeedReduction = 3;
+        slowEffect.PowerUpDuration = 1;
+        slowEffect.SetScaleXY(0.5f);
+
+
+        GameManager.Instance.ActiveLevel.AddChildAt(slowEffect, GameManager.Instance.ActiveLevel.GetChildren().Count - 10);
+    }
+
+}
--- ArcadeFiles/PowerUps/AcidBottle.cs
+++ ArcadeFiles/PowerUps/AcidBottle.cs
@@ -32,15 +32,26 @@ class AcidBottle:PowerUp
         Console.WriteLine(this+ "Power Up used");
 
         var position = TransformPoint(x, y);
-        var slowEffect = new SlowEffect("Art/puddle.png", 2, 1);
+
+
+        Bottle bottle = new Bottle("Art/acid.png",2,1);
+        bottle.SetScaleXY(0.5f);
+
+        bottle.SetPivotPoint(PivotPointPosition.BOTTOM);
+        bottle.SetXY(owner.Owner.x - 150, owner.Owner.y - 10);
+
+        GameManager.Instance.ActiveLevel.AddChildAt(bottle, GameManager.Instance.ActiveLevel.GetChildren().Count - 10);
+
+        /*var slowEffect = new SlowEffect("Art/puddle.png", 2, 1);
         slowEffect.SetPivotPoint(PivotPointPosition.BOTTOM);
         
         slowEffect.SetXY(owner.Owner.x-150, owner.Owner.y+30);
         //slowEffect.SpeedReduction = 3;
         slowEffect.PowerUpDuration = 1;
         slowEffect.SetScaleXY(0.5f);
+        */
 
-        GameManager.Instance.ActiveLevel.AddChildAt(slowEffect,GameManager.Instance.ActiveLevel.GetChildren().Count - 10);
+        //GameManager.Instance.ActiveLevel.AddChildAt(slowEffect,GameManager.Instance.ActiveLevel.GetChildren().Count - 10);
 
         this.LateDestroy();
     }
